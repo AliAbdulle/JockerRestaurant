eb6d05abbf24d1595d849a37a5f41ac3a0e5207d
AliAbdulle <alidhobey@gmail.com>
2019-10-19 21:43:32 -0500

    Adding restaurant  data

--- JockerRestaurant/Models/Restaurant.cs
+++ JockerRestaurant/Models/Restaurant.cs
@@ -7,5 +7,11 @@ namespace JockerRestaurant.Models
 {
     public class Restaurant
     {
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+  
+        public string City{ get; set; }
+
     }
 }
