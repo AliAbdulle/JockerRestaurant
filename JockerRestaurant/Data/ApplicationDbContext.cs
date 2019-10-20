f20fcfa6a3cc04a2f366b0a96f1b4952cc286a2c
AliAbdulle <alidhobey@gmail.com>
2019-10-19 21:50:45 -0500

    add restaurant controller

--- JockerRestaurant/Data/ApplicationDbContext.cs
+++ JockerRestaurant/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using JockerRestaurant.Models;
 
 namespace JockerRestaurant.Data
 {
@@ -12,5 +13,6 @@ namespace JockerRestaurant.Data
             : base(options)
         {
         }
+        public DbSet<JockerRestaurant.Models.Restaurant> Restaurant { get; set; }
     }
 }
