21a37e43bc5b7ab5bd09e30d68f3c4cd211da8c4
AliAbdulle <alidhobey@gmail.com>
2019-10-29 19:15:03 -0500

    added image data

--- JockerRestaurant/Models/Menu.cs
+++ JockerRestaurant/Models/Menu.cs
@@ -10,5 +10,6 @@ namespace JockerRestaurant.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Types { get; set; }
+        public string Image { get; set }
     }
 }
